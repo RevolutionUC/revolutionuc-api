eac9307aebf217da2f8bcb82841f57786e1bd4b7
Isiah Lloyd <robotdude101@gmail.com>
2018-12-01 14:55:37 -0500

    Fix database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,7 @@ import { Registrant } from 'entities/registrant.entity';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'postgres',
-    host: environment.database_config.host,
-    port: environment.database_config.port,
-    username: environment.database_config.username,
-    password: environment.database_config.password,
+    url: environment.database_config.url,
     database: environment.database_config.database,
     entities: [
       'src/**/**.entity{.ts,.js}',
