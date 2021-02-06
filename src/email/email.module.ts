e4a4b32689058a45107970a71af685ecaa92bf5b
Dev Agrawal <devagrawal09@gmail.com>
2021-02-06 04:00:17 -0500

    fixed some broken imports

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Registrant } from '../entities/registrant.entity';
 import { EmailService } from './email.service';
 import { AuthModule } from '../auth/auth.module';
+import { Judge } from 'src/judging/entities/judge.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Registrant]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Registrant, Judge]), AuthModule],
   providers: [EmailService],
   exports: [EmailService]
 })
