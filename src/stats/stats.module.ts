9ddd75c28ba4e8d7d854a26c5cd9d961fd78883d
Dev Agrawal <devagrawal09@gmail.com>
2021-12-30 19:18:58 -0500

    added entity to stats module

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,12 +1,14 @@
 import { HttpModule, Module } from "@nestjs/common";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Registrant } from '../entities/registrant.entity';
+import { Hacker } from "../lattice/entities/hacker.entity";
+import { Notification } from "../lattice/entities/notification.entity";
 import { DailyUpdateService } from './daily-update.service';
 import { StatsController } from './stats.controller';
 import { StatsService } from './stats.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Registrant]), HttpModule],
+  imports: [TypeOrmModule.forFeature([Registrant, Hacker, Notification]), HttpModule],
   controllers: [StatsController],
   providers: [StatsService, DailyUpdateService],
 })
