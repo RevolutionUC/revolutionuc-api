38c290d8c5cf20d404700a3444302307ebacac96
Zenith Zack <faldund@outlook.com>
2022-12-26 23:24:43 -0600

    (fix): bypassing cors by setting response header

--- src/app.service.ts
+++ src/app.service.ts
@@ -139,8 +139,7 @@ export class AppService {
       if (err) {
         throw new HttpException('There was an error uploading the resume', 500);
       } else {
-        res.set("Access-Control-Allow-Origin", "*");
-        return res.status(HttpStatus.CREATED).send();
+        return res.set("Access-Control-Allow-Origin", "*").status(HttpStatus.CREATED).send();
       }
     });
   }
